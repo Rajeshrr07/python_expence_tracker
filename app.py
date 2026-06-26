60e4a71a86ba89b2c147680dbe888d2e9429ef61
Rajesh <rajeshoffice07@gmail.com>
2026-06-26 10:48:07 +0530

    modify app.py file for the deployment

--- app.py
+++ app.py
@@ -83,5 +83,6 @@ def handle_budget():
 
     return jsonify({'budget': read_budget()})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
